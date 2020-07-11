c9c8ec18369256341809ac390a3838248f105df9
Sitnikov-Ryslan <63663502+Sitnikov-Ryslan@users.noreply.github.com>
2020-07-11 23:26:25 +0400

    Add files via upload

--- JS/toggleForms.js
+++ JS/toggleForms.js
@@ -3,12 +3,14 @@ let loginToggleBtn = document.querySelector('.form_login-subtitle__link');
 let accForm = document.querySelector('.form_account');
 let logForm = document.querySelector('.form_login');
 
-accountToggleBtn.addEventListener('click', function() {
+accountToggleBtn.addEventListener('click', function(evt) {
+	evt.preventDefault();
 	accForm.classList.add('hidden');
 	logForm.classList.remove('hidden');
 });
 
-loginToggleBtn.addEventListener('click', function() {
+loginToggleBtn.addEventListener('click', function(evt) {
+	evt.preventDefault();
 	logForm.classList.add('hidden');
 	accForm.classList.remove('hidden');
 });
